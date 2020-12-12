809a4814cf8c342687ece91bb4914d2f67f1f955
avalon <amyunfei@163.com>
2020-12-12 20:36:08 +0800

    test

--- frontend/exhibition/src/views/admin/admin.tsx
+++ frontend/exhibition/src/views/admin/admin.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import style from './admin.module.scss'
+import classnames from 'classnames/bind'
+
+const cx = classnames.bind(style)
 
 const Admin: React.FC = () => {
   return (
-    <div>
-      123
+    <div className={cx('mr-admin-index')}>
+      div
     </div>
   )
 }
